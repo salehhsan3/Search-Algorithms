b3b790a63eb9dc8598f692e5a90c016651627640
badernatour1211 <132554553+badernatour1211@users.noreply.github.com>
2023-05-09 10:28:23 +0300

    Add files via upload

--- Algorithms.py
+++ Algorithms.py
@@ -11,73 +11,153 @@ class GraphNode:
         self.parent = parent
         self.action = action
         self.cost = cost
-        self.in_OPEN = False
-        self.in_CLOSE = False
+        self.g_value = 0
+        self.h_value = 0
+        self.weight = 0
 
-
-
-class BFSAgent():
+class Agent:
     def __init__(self) -> None:
-        self.OPEN = queue.SimpleQueue()  #FIFO QUEUE
-        self.CLOSE = set()
+        self.OPEN_SET = {}
+        self.CLOSE = {}
         self.expanded_counter = 0.0
+    def get_path(self,goal_node: GraphNode,expanded) -> Tuple[List[int] ,int, float]:
+        lst = []
+        node_iter = goal_node
+        cost = 0
+        while node_iter.parent is not None:
+            lst.insert(0,node_iter.action)
+            cost += node_iter.cost
+        return (lst,cost,expanded)
 
-    def get_path(self,node: GraphNode) -> Tuple[List[int],int,float]:
-        pass
-########################################################################################3
-    def exist_in_queue(self,q,state):
-        #######################33
-       pass
+    def h_MSAP(self,G: set, state: int):
+        env_ = FrozenLakeEnv()
+        state_x, state_y = env_.to_row_col()
+        Manhattan_dist = env_.inf
+        for g in G:
+            goal_x, goal_y = env_.to_row_col()
 
+
+class BFSAgent(Agent):
+    def __init__(self) -> None:
+        self.OPEN = queue.SimpleQueue()
+        super().__init__()
+
+       # self.OPEN_SET = set()
+       # self.CLOSE = set()
+       # self.expanded_counter = 0.0
 
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
         initial_node = GraphNode(env.s,None,None,None)
-        initial_node.in_OPEN = True
+        if env.is_final_state(initial_node.state):
+            return ([],0,0)
         self.OPEN.put(initial_node)
+        self.OPEN_SET[initial_node.state] = initial_node
 
         while not self.OPEN.empty():
             node_to_expand = self.OPEN.get()
+            del self.OPEN_SET[node_to_expand.state]
             self.expanded_counter += 1
-            self.CLOSE.add(node_to_expand.state)
-            node_to_expand.in_CLOSE = True
-            node_to_expand.in_OPEN = False
+            self.CLOSE[node_to_expand.state] = node_to_expand
 
             for act,tup in env.succ(node_to_expand.state).items():
                 next_state, cost, terminated = tup
                 if terminated:
                     if env.is_final_state(next_state):
                         final_node = GraphNode(next_state, node_to_expand, act, cost)
-                        return self.get_path(final_node)
-                if self.exist_in_queue(self.OPEN,next_state) or next_state in self.CLOSE:
+                        return self.get_path(final_node,self.expanded_counter)
+                    else:
+                        pass # this is A HOLE
+                if (next_state in self.OPEN_SET) or (next_state in self.CLOSE):
                     pass
                 else:
                     new_node = GraphNode(next_state,node_to_expand,act,cost)
                     self.OPEN.put(new_node)
-
-
-
-
-
+                    self.OPEN_SET[next_state] = new_node
 
 
 
 
 class DFSAgent():
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.OPEN = queue.LifoQueue()
+        super().__init__()
+
+    # self.OPEN_SET = set()
+    # self.CLOSE = set()
+    # self.expanded_counter = 0.0
 
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
-        raise NotImplementedError
-        
+        initial_node = GraphNode(env.s, None, None, None)
+        if env.is_final_state(initial_node.state):
+            return ([],0,0)
+        self.OPEN.put(initial_node)
+        self.OPEN_SET[initial_node.state] = initial_node
+
+        while not self.OPEN.empty():
+            node_to_expand = self.OPEN.get()
+            del self.OPEN_SET[node_to_expand.state]
+            self.expanded_counter += 1
+            self.CLOSE[node_to_expand.state] = node_to_expand
+
+            for act, tup in env.succ(node_to_expand.state).items():
+                next_state, cost, terminated = tup
+                if terminated:
+                    if env.is_final_state(next_state):
+                        final_node = GraphNode(next_state, node_to_expand, act, cost)
+                        return self.get_path(final_node, self.expanded_counter)
+                    else:
+                        pass  # this is A HOLE
+                if (next_state in self.OPEN_SET) or (next_state in self.CLOSE):
+                    pass
+                else:
+                    new_node = GraphNode(next_state, node_to_expand, act, cost)
+                    self.OPEN.put(new_node)
+                    self.OPEN_SET[next_state] = new_node
 
 
-class UCSAgent():
-  
+class UCSAgent(Agent):
+
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.OPEN = queue.PriorityQueue()
+        super().__init__()
 
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
-        raise NotImplementedError
+        initial_node = GraphNode(env.s, None, None, None)
+        if env.is_final_state(initial_node.state):
+            return ([], 0, 0)
+        self.OPEN.put((initial_node.g_value,initial_node))
+        self.OPEN_SET[initial_node.state] = initial_node
+
+        while not self.OPEN.empty():
+            node_to_expand_idx, node_to_expand = self.OPEN.get()
+            del self.OPEN_SET[node_to_expand.state]
+            self.CLOSE[node_to_expand.state] = node_to_expand
+            if env.is_final_state(node_to_expand.state):
+                return self.get_path(node_to_expand,self.expanded_counter)
+            self.expanded_counter += 1
+
+            for act, tup in env.succ(node_to_expand.state).items():
+                next_state, cost, terminated = tup
+                 if (next_state not in self.OPEN_SET) or (next_state not in self.CLOSE):
+                     new_node = GraphNode(next_state,node_to_expand,act,cost)
+                     new_node.g_value = node_to_expand.g_value + cost
+                     self.OPEN.put((new_node.g_value,new_node))
+                     self.OPEN_SET[next_state] = new_node
+                 elif next_state in self.OPEN_SET:
+                     next_node = self.OPEN_SET[next_state]
+                     new_cost = node_to_expand.g_value + cost
+                     if new_cost < next_node.g_value:
+                         next_node.g_value = new_cost
+                         next_node.parent = node_to_expand
+                         next_node.action = act
+                         next_node.cost = cost
+
+
+
+
+
+
+
 
 
 
