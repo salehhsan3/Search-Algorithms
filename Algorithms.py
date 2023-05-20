d90a926ad9cd4f0c4d2ef664eb7d39fbb7a5a5be
unknown <salehhsan3@gmail.com>
2023-05-20 11:24:01 +0300

    saved algorithms progress - saleh

--- Algorithms.py
+++ Algorithms.py
@@ -128,9 +128,6 @@ class BFSAgent(Agent):
             if None not in env.succ(node_to_expand.state)[0]:
                 for act, tup in env.succ(node_to_expand.state).items():
                     next_state,  cost,  terminated = tup
-                    # print(str(env.succ(node_to_expand.state).items()))
-                    # print("next-state is " + str(next_state) + " and cost is :" + str(cost) + " and terminated is " + str(terminated) )
-                    # print((str(tup in env.succ(node_to_expand.state))))
                     if terminated:
                         if env.is_final_state(next_state):
                             final_node = GraphNode(next_state,  node_to_expand,  act,  cost)
@@ -147,52 +144,80 @@ class DFSAgent(Agent):
     def __init__(self) -> None:
         self.OPEN = queue.LifoQueue()
         super().__init__()
-
+        
+    def search_aux(self, env: FrozenLakeEnv) -> Tuple[List[int],  int,  float]:
+        node_to_expand = self.OPEN.get()
+        del self.OPEN_SET[node_to_expand.state]
+        self.CLOSE[node_to_expand.state] = node_to_expand
+        if env.is_final_state(node_to_expand.state):
+            final_node = GraphNode(node_to_expand,  node_to_expand,  act,  cost)
+            return self.get_path(final_node,  self.expanded_counter)
+        self.expanded_counter += 1 # after adding node to CLOSE, even HOLE node are added (we expanded the empty set)
+        if None in env.succ(node_to_expand.state)[0]:
+            return [], 0 ,0 # this is a HOLE and there's no need to expand it, return EMPTY list to say didn't reach destination
+        for act,  tup in env.succ(node_to_expand.state).items(): 
+            next_state,  cost,  terminated = tup
+            if terminated:
+                if env.is_final_state(next_state):
+                    final_node = GraphNode(next_state,  node_to_expand,  act,  cost)
+                    return self.get_path(final_node,  self.expanded_counter)
+                # else:
+                #     continue  # this is A HOLE don't insert it to the OPEN list
+            if (next_state in self.OPEN_SET) or (next_state in self.CLOSE):
+                continue
+            else:
+                new_node = GraphNode(next_state,  node_to_expand,  act,  cost)
+                self.OPEN.put(new_node)
+                self.OPEN_SET[next_state] = new_node
+                res = self.search_aux(env) # deepen the seach
+                if bool(res[0]): # checking if we have a list with length because in cases of failure we return an empty list with length = 0
+                    return res
+        return [], np.inf, self.expanded_counter # FAILURE
+        
     def search(self,  env: FrozenLakeEnv) -> Tuple[List[int],  int,  float]:
         initial_node = GraphNode(env.reset(),  None,  None,  None)
         if env.is_final_state(initial_node.state):
-            return ([], 0, 0)
+            return ([], 0, 0) # the empty solution
         self.OPEN.put(initial_node)
         self.OPEN_SET[initial_node.state] = initial_node
-
-        while not self.OPEN.empty():
-            node_to_expand = self.OPEN.get()
-            del self.OPEN_SET[node_to_expand.state]
-            self.expanded_counter += 1
-            self.CLOSE[node_to_expand.state] = node_to_expand
-            if None in env.succ(node_to_expand.state)[0]:   # we're expanding a Hole, so we have to return
-                return ([], np.inf, self.expanded_counter) # to check if this is a valid return as "failure"!!!!!!!!!!!!!!!!!!!!!
-            for act,  tup in env.succ(node_to_expand.state).items():
-                next_state,  cost,  terminated = tup
-                if terminated:
-                    if env.is_final_state(next_state):
-                        final_node = GraphNode(next_state,  node_to_expand,  act,  cost)
-                        return self.get_path(final_node,  self.expanded_counter)
-                    else:
-                        pass  # this is A HOLE don't insert it to the OPEN list
-                if (next_state in self.OPEN_SET) or (next_state in self.CLOSE):
-                    pass
-                else:
-                    new_node = GraphNode(next_state,  node_to_expand,  act,  cost)
-                    self.OPEN.put(new_node)
-                    self.OPEN_SET[next_state] = new_node
+        return self.search_aux(env)
+        # while not self.OPEN.empty():
+        #     node_to_expand = self.OPEN.get()
+        #     del self.OPEN_SET[node_to_expand.state]
+        #     self.expanded_counter += 1
+        #     self.CLOSE[node_to_expand.state] = node_to_expand
+            # problem : with this implementation we behave like BFS we expand all of the nodes of the same depth before moving forward
+        #               to solve that I had to use recursion in order to keep the solution simple.
+        #     for act,  tup in env.succ(node_to_expand.state).items(): 
+        #         # if act == None or tup[0] == None:
+        #         #     continue # we've met a HOLE should contune but maybe it has to enter expanded counter
+        #         next_state,  cost,  terminated = tup
+        #         if terminated:
+        #             if env.is_final_state(next_state):
+        #                 final_node = GraphNode(next_state,  node_to_expand,  act,  cost)
+        #                 return self.get_path(final_node,  self.expanded_counter)
+        #             else:
+        #                 continue  # this is A HOLE don't insert it to the OPEN list
+        #         if (next_state in self.OPEN_SET) or (next_state in self.CLOSE):
+        #             continue
+        #         else:
+        #             new_node = GraphNode(next_state,  node_to_expand,  act,  cost)
+        #             self.OPEN.put(new_node)
+        #             self.OPEN_SET[next_state] = new_node
 class UCSAgent(Agent):
 
     def __init__(self) -> None:
-        # self.OPEN = PriorityQueue()
         self.OPEN = heapdict.heapdict() # essentially a priority queue for our purposes
         super().__init__()
 
     def search(self,  env: FrozenLakeEnv) -> Tuple[List[int],  int,  float]:
         initial_node = GraphNode(env.reset(),  None,  None,  None)
         if env.is_final_state(initial_node.state):
-            return ([],  0,  0)
-        # self.OPEN.put((initial_node.g_value, initial_node))
+            return ([],  0,  0) # return the empty path
         self.OPEN[NodeKey(initial_node.g_value, initial_node.state)] = initial_node
         self.OPEN_SET[initial_node.state] = initial_node
 
         while bool(self.OPEN): # meaning heap is not empty
-            # node_to_expand_idx,  node_to_expand = self.OPEN.get()
             node_to_expand_idx,  node_to_expand = self.OPEN.popitem()
             del self.OPEN_SET[node_to_expand.state]
             self.CLOSE[node_to_expand.state] = node_to_expand
@@ -200,25 +225,26 @@ class UCSAgent(Agent):
                 return self.get_path(node_to_expand, self.expanded_counter)
             self.expanded_counter += 1
             
-            if None not in env.succ(node_to_expand.state)[0]:
-                for act,  tup in env.succ(node_to_expand.state).items():
-                    next_state,  cost,  terminated = tup
-                    if terminated and not env.is_final_state(next_state):
-                        continue
-                    if (next_state not in self.OPEN_SET) and (next_state not in self.CLOSE):
-                        new_node = GraphNode(next_state, node_to_expand, act, cost)
-                        new_node.g_value = node_to_expand.g_value + cost
-                        #  self.OPEN.put((new_node.g_value, new_node))
-                        self.OPEN[NodeKey(new_node.g_value, new_node.state)] = new_node
-                        self.OPEN_SET[next_state] = new_node
-                    elif next_state in self.OPEN_SET:
-                        next_node = self.OPEN_SET[next_state]
-                        new_cost = node_to_expand.g_value + cost
-                        if new_cost < next_node.g_value:
-                            next_node.g_value = new_cost
-                            next_node.parent = node_to_expand
-                            next_node.action = act
-                            next_node.cost = cost
+            # if None not in env.succ(node_to_expand.state)[0]:
+            for act,  tup in env.succ(node_to_expand.state).items():
+                if tup[0] == None: # meaning this is a HOLE
+                    continue
+                next_state,  cost,  terminated = tup
+                # if terminated and not env.is_final_state(next_state):
+                #     continue
+                if (next_state not in self.OPEN_SET) and (next_state not in self.CLOSE):
+                    new_node = GraphNode(next_state, node_to_expand, act, cost)
+                    new_node.g_value = node_to_expand.g_value + cost
+                    self.OPEN[NodeKey(new_node.g_value, new_node.state)] = new_node
+                    self.OPEN_SET[next_state] = new_node
+                elif next_state in self.OPEN_SET:
+                    next_node = self.OPEN_SET[next_state]
+                    new_cost = node_to_expand.g_value + cost
+                    if new_cost < next_node.g_value:
+                        next_node.g_value = new_cost
+                        next_node.parent = node_to_expand
+                        next_node.action = act
+                        next_node.cost = cost
 class GreedyAgent(Agent):
   
     def __init__(self) -> None:
@@ -307,10 +333,42 @@ class WeightedAStarAgent(Agent):
                         self.OPEN[NodeKey(curr_node.f_value,curr_node.state)] = curr_node
                         self.OPEN_SET[next_state] = curr_node
                         del self.CLOSE[next_state]
-class IDAStarAgent():
-    def __init__(self) -> None:
-        self.OPEN = heapdict.heapdict() # essentially a priority queue for our purposes
-        super().__init__()
+# class IDAStarAgent(Agent):
+#     def __init__(self) -> None:
+#         self.OPEN = heapdict.heapdict() # essentially a priority queue for our purposes
+#         super().__init__()
+    
+#     def DFS-f(self, env : FrozenLakeEnv, node: GraphNode, Goals: List[int], g_value, f_limit: float ) -> Tuple[List[int],  int,  float]:
+#         new_f_value = g_value + self.h_MSAP(env, Goals, node.state)
+#         if new_f_value > f_limit:
+#             new_limit = min(new_limit,new_f)
+#             return [], -1, -1 # returns failure
+#         if env.is_final_state(node.state):
+#                 return self.get_path(node, self.expanded_counter)
+#         for act,  tup in env.succ(node.state).items():
+#             if tup[1] is None or act is None: # tup[1] is cost
+#                 continue
+#             next_state,  cost,  terminated = tup
+#             new_node = GraphNode(next_state, node, act, cost) 
+#             actions, total_cost, expanded = DFS-f(env, new_node, Goals, (g_value + cost), f_limit  )
+#             if expanded >= 0: # in case we fail we'll set expanded to -1 (result != failure check)
+#                 return actions, total_cost, expanded
+            
+#         return [], -1, -1 # check on piazza what exactly should we return in cases of failure
+
+#     def search(self,  env: FrozenLakeEnv) -> Tuple[List[int],  int,  float]:
+#         initial_node = GraphNode(env.reset(),  None,  None,  None)
+#         Goals = env.goals
+#         if env.is_final_state(initial_node.state):
+#             return ([],  0,  0)
+#         global new_limit = h_MSAP(env, Goals, initial_node.state) # this is a global variable
         
-    def search(self,  env: FrozenLakeEnv) -> Tuple[List[int],  int,  float]:
-        raise NotImplementedError
+#         while not InterruptedError:
+#             new_limit = f_limit
+#             new_limit = np.inf
+#             actions, total_cost, expanded = DFS-f()
+#             if expanded >= 0: # in case we fail we'll set expanded to -1 (result != failure check)
+#                 return actions, total_cost, expanded
+#         return [], -1, -1 #check on piazza what exactly should we return in cases of failure
+
+        
